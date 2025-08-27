25575b405de6317677c11006a4aee09e69bd15cd
Anshu Panwar <anshumona2005@gmail.com>
2025-08-27 12:52:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from nltk.stem.porter import PorterStemmer
 
 # Download necessary NLTK resources
 nltk.download('punkt')
-nltk.download('stopwords'
+nltk.download('stopwords')
 
 # Load model and vectorizer
 ps = PorterStemmer()
